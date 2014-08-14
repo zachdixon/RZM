7e26c47ec3ffc1c8292362dc99b8dbef1aacd675
Zachary Dixon <zach@socialcompass.net>
2014-08-14 02:14:38 -0500

    Add nav icons, update edit profile

--- js/edit_profile.js
+++ js/edit_profile.js
@@ -1,14 +1,23 @@
 $(function(){
+  $('.sub-nav-link').on('click', changeSection);
+  $('#game-face-upload').on('change', changeGameFace);
+  $('#winning-face-upload').on('change', changeWinningFace);
 });
 
-function changeGameFace(e) {
-  var f, files, i, reader;
+function changeSection(e) {
+  e.preventDefault();
+  var $btn, $section;
+  $btn = $(e.currentTarget);
+  $('.main-sub-nav li.active').removeClass('active');
+  $btn.closest('li').addClass('active');
+  $section = $('.'+$btn.attr('data-section'));
+  $('.settings-section:visible').fadeOut('fast', function() {
+    $($section).show();
+  });
+}
 
-  files = e.target.files;
-
-  i = 0;
-
-  f = void 0;
+function getImage(e, img, input) {
+  var files = e.target.files, f = undefined, reader, i = 0;
 
   while (f = files[i]) {
     if (!f.type.match("image.*")) {
@@ -17,11 +26,17 @@ function changeGameFace(e) {
     reader = new FileReader();
     reader.onload = (function(theFile) {
       return function(e) {
-        var img;
-        return img = e.target.result;
+        $(img).attr('src', e.target.result);
+        $(input).val(e.target.result);
       };
     })(f);
     reader.readAsDataURL(f);
     i++;
   }
+}
+function changeGameFace(e) {
+  var image = getImage(e, '.img-game-face', '#game-face-hidden');
+}
+function changeWinningFace(e) {
+  var image = getImage(e, '.img-winning-face', '#winning-face-hidden');
 }
