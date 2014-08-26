1fa30b9cb4e138cd2a16b3e1161524058d6421b8
zachdixon <rissk13@gmail.com>
2014-08-26 17:20:41 -0500

    Added lms games, monkey points, and new layout to commissioner panel

--- js/commissioner_panel.js
+++ js/commissioner_panel.js
@@ -3,10 +3,39 @@ $(function(){
   startLoading();
   imagesLoaded(function(){
     finishedLoading();
+    showHideLMS();
   });
+  $('.sub-nav-link').on('click', changeSection);
   $('.select2').select2({tags: [], dropdownCssClass: 'hide'});
-  $('.ladda-button').ladda('bind');
+  $('[data-toggle="tooltip"]').tooltip();
+  $('.radio-league-type').on('change', showHideLMS);
+
+//  $('.ladda-button').ladda('bind');
 });
+
+function changeSection(e) {
+  e.preventDefault();
+  var $btn, $section;
+  $btn = $(e.currentTarget);
+  $('.main-sub-nav li.active').removeClass('active');
+  $btn.closest('li').addClass('active');
+  $section = $('.'+$btn.attr('data-section'));
+  $('.tab-section:visible').fadeOut('fast', function() {
+    $($section).show();
+  });
+}
+
+function showHideLMS(e) {
+  e.preventDefault();
+  var $input, $lms = $('.lms-games');
+  $input = $(e.currentTarget);
+  if($input.val() == 'plus') {
+    $lms.show();
+  } else {
+    $lms.hide();
+  }
+
+}
 
 function imagesLoaded(){
   var $images = $('img'), images_loaded = 0;
