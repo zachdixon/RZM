1801f0d85d9d3ab4a93868e28d88c53c9835017b
zachdixon <rissk13@gmail.com>
2014-08-27 22:41:04 -0500

    Fixed height issue on player avatar images and resize base64 images to max width of 300px

--- js/edit_profile.js
+++ js/edit_profile.js
@@ -26,8 +26,12 @@ function getImage(e, img, input) {
     reader = new FileReader();
     reader.onload = (function(theFile) {
       return function(e) {
-        $(img).attr('src', e.target.result);
-        $(input).val(e.target.result);
+        var i = new Image(), resizedImg, cwidth = 300, cheight;
+        i.src = e.target.result;
+        cheight = cwidth * (i.height / i.width);
+        resizedImg = imageToDataUri(i, cwidth, cheight);
+        $(img).attr('src', resizedImg);
+        $(input).val(resizedImg);
       };
     })(f);
     reader.readAsDataURL(f);
@@ -39,4 +43,20 @@ function changeGameFace(e) {
 }
 function changeWinningFace(e) {
   var image = getImage(e, '.img-winning-face', '#winning-face-hidden');
+}
+function imageToDataUri(img, width, height) {
+
+  /// create an off-screen canvas
+  var canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d');
+
+  /// set its dimension to target size
+  canvas.width = width;
+  canvas.height = height;
+
+  /// draw source image into the off-screen canvas:
+  ctx.drawImage(img, 0, 0, width, height);
+
+  /// encode image to data-uri with base64 version of compressed image
+  return canvas.toDataURL();
 }
