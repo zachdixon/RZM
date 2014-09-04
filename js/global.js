3aabeb3413d35a818d15f7adc2d716d6cc292e8c
zachdixon <rissk13@gmail.com>
2014-09-03 21:04:28 -0500

    Updated site with new navs and headers

--- js/global.js
+++ js/global.js
@@ -33,3 +33,73 @@ function finishedLoading(){
     $('#main-container').show();
   });
 }
+
+
+//######################################################################################
+// Author: ricocheting.com
+// Version: v2.0
+// Date: 2011-03-31
+// Description: displays the amount of time until the "dateFuture" entered below.
+
+// NOTE: the month entered must be one less than current month. ie; 0=January, 11=December
+// NOTE: the hour is in 24 hour format. 0=12am, 15=3pm etc
+// format: dateFuture1 = new Date(year,month-1,day,hour,min,sec)
+// example: dateFuture1 = new Date(2003,03,26,14,15,00) = April 26, 2003 - 2:15:00 pm
+
+dateFuture1 = new Date(2014,9,2,15,54,00);
+
+// TESTING: comment out the line below to print out the "dateFuture" for testing purposes
+//document.write(dateFuture +"<br />");
+
+
+//###################################
+//nothing beyond this point
+function GetCount(ddate,iid){
+
+  dateNow = new Date();	//grab current date
+  amount = ddate.getTime() - dateNow.getTime();	//calc milliseconds between dates
+  delete dateNow;
+
+  // if time is already past
+  if(amount < 0){
+    document.getElementById(iid).innerHTML="Now!";
+  }
+  // else date is still good
+  else{
+    days=0;hours=0;mins=0;secs=0;out="";
+
+    amount = Math.floor(amount/1000);//kill the "milliseconds" so just secs
+
+    days=Math.floor(amount/86400);//days
+    amount=amount%86400;
+
+    hours=Math.floor(amount/3600);//hours
+    amount=amount%3600;
+
+    mins=Math.floor(amount/60);//minutes
+    amount=amount%60;
+
+    secs=Math.floor(amount);//seconds
+
+    if(days != 0){out += "<p class='pull-left'><span>" + days +"</span>"+((days==1)?"day":"days")+"</p>";}
+    if(hours != 0){out += "<p class='pull-left'><span>" + hours +"</span>"+((hours==1)?"hour":"hours")+"</p>";}
+    out += "<p class='pull-left'><span>" + mins +"</span>"+((mins==1)?"min":"mins")+"</p>";
+    out += "<p class='pull-left'><span>" + secs +"</span>"+((secs==1)?"sec":"secs")+"</p>";
+    document.getElementById(iid).innerHTML=out;
+
+    // No labels
+//    if(days != 0){out += days +":";}
+//    if(hours != 0){out += hours +":";}
+//    out += mins +":";
+//    out += secs +":";
+//    out = out.substr(0,out.length-1);
+//    document.getElementById(iid).innerHTML=out;
+
+    setTimeout(function(){GetCount(ddate,iid)}, 1000);
+  }
+}
+
+window.onload=function(){
+  GetCount(dateFuture1, 'countbox1');
+  //you can add additional countdowns here (just make sure you create dateFuture2 and countbox2 etc for each)
+};
